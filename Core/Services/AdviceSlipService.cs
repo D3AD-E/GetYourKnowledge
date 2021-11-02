d95870c9fabe823881bf8d0c05746412e9c76450
D3AD <evg.gordi@gmail.com>
2021-11-02 15:48:06 +0100

    Initial creation of services

---
+++ Core/Services/AdviceSlipService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GetYourKnowledge.Core.Services
+{
+    /// <summary>
+    /// AdviceSlipService is used for getting JSON of advices in English
+    /// </summary>
+    public class AdviceSlipService
+    {
+        public HttpClient Client { get; }
+    }
+}
---
+++ Core/Services/LibreTranslateService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GetYourKnowledge.Core.Services
+{
+    /// <summary>
+    /// LibreTranslateService is used for translating advices from English to Polish
+    /// </summary>
+    public class LibreTranslateService
+    {
+        public HttpClient Client { get; }
+    }
+}
